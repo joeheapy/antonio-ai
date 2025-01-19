12f52828d268569f1329894f6d6c0ce482ad70ff
joeheapy <joe.heapy@me.com>
2025-01-19 17:25:39 +0000

    Preparing Clerk

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,5 @@
+const DashboardPage = () => {
+  return <div className="text-4xl">Dashboard Page (Protected)</div>
+}
+
+export default DashboardPage
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,4 @@
+const LandingPage = () => {
+  return <div className="text-4xl">Unprotected Page</div>
+}
+export default LandingPage
